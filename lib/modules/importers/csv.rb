562451efa28fae222bba1c99076635c78e1665f4
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-05-11 17:31:45 +0100

    add CSV importer and initial seeds

---
+++ lib/modules/importers/csv.rb
@@ -0,0 +1,65 @@
+require 'csv'
+
+module Importers::Csv
+  def self.import(path, wdpa_release=nil)
+    wdpa_release = detect_release(path) unless wdpa_release
+
+    ActiveRecord::Base.transaction do
+      CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
+        protected_area = find_protected_area(row[:wdpaid])
+        next if protected_area.nil?
+
+        protected_area.wdpa_releases << wdpa_release
+        protected_area.countries     = find_countries(row[:iso3])
+        protected_area.designation   = find_designation(row[:desig_eng], row[:desig_type])
+        protected_area.save!
+      end
+    end
+  end
+
+  def self.find_protected_area(wdpa_id)
+    return nil if wdpa_id.blank?
+    ProtectedArea.find_or_initialize_by(wdpa_id: wdpa_id)
+  end
+
+  def self.find_countries(isos3)
+    return [] if isos3.blank?
+
+    isos3.split(";").map do |iso3|
+      Country.find_or_initialize_by(iso3: iso3)
+    end
+  end
+
+  def self.find_designation(name, type)
+    return nil if name.blank? || type.blank?
+    designation_type = DesignationType.find_or_initialize_by(name: type)
+    designation = Designation.find_or_initialize_by(name: name, designation_type: designation_type)
+
+    designation
+  end
+
+  def self.detect_release(path)
+    filename = File.basename(path, ".csv")
+
+    if filename =~ /year-(\d{4})/
+      year = $1.to_i
+
+      return WdpaRelease.create(
+        name:       "WDPA_#{$1}",
+        valid_from: DateTime.new(year),
+        valid_to:   DateTime.new(year) + 1.year
+      )
+    end
+
+    if filename =~ /month-(\d{2})-(\d{4})/
+      month = $1.to_i
+      year  = $2.to_i
+
+      return WdpaRelease.create(
+        name:       "WDPA_#{$1}",
+        valid_from: DateTime.new(year, month),
+        valid_to:   DateTime.new(year, month) + 1.month
+      )
+    end
+  end
+end
---
+++ test/lib/modules/importers/csv_test.rb
@@ -0,0 +1,58 @@
+require 'csv'
+require 'test_helper'
+
+class ImportersCsvTest < ActiveSupport::TestCase
+  CSV_CONTENT = [{wdpaid: "123", iso3: "CIV", desig_eng: "National Park", desig_type: "National"},
+                 {wdpaid: "234", iso3: "CIV", desig_eng: "National Park", desig_type: "National"}]
+
+  test "import(path, wdpa_release) creates protected areas with wdpa_id" do
+    path = "tmp/uploaded.csv"
+    wdpa_release = FactoryGirl.create(:wdpa_release)
+    CSV.stubs(:foreach).multiple_yields([CSV_CONTENT[0]], [CSV_CONTENT[1]])
+
+    Importers::Csv.import(path, wdpa_release)
+
+    assert ProtectedArea.find_by_wdpa_id(123), "pa with 123 not found"
+    assert ProtectedArea.find_by_wdpa_id(234), "pa with 234 not found"
+  end
+
+  test "import(path, wdpa_release) creates protected areas with wdpa_release" do
+    path = "tmp/uploaded.csv"
+    wdpa_release = FactoryGirl.create(:wdpa_release)
+    CSV.stubs(:foreach).multiple_yields([CSV_CONTENT[0]], [CSV_CONTENT[1]])
+
+    Importers::Csv.import(path, wdpa_release)
+
+    assert_equal [wdpa_release], ProtectedArea.find_by_wdpa_id(123).wdpa_releases
+    assert_equal [wdpa_release], ProtectedArea.find_by_wdpa_id(234).wdpa_releases
+  end
+
+  test "import(path, wdpa_release) creates protected areas with the country" do
+    path = "tmp/uploaded.csv"
+    country = FactoryGirl.create(:country, iso3: "CIV")
+    wdpa_release = FactoryGirl.create(:wdpa_release)
+
+    CSV.stubs(:foreach).multiple_yields([CSV_CONTENT[0]], [CSV_CONTENT[1]])
+
+    Importers::Csv.import(path, wdpa_release)
+
+    assert_equal [country], ProtectedArea.find_by_wdpa_id(123).countries
+    assert_equal [country], ProtectedArea.find_by_wdpa_id(234).countries
+  end
+
+  test "import(path, wdpa_release) creates protected areas with the designation" do
+    path = "tmp/uploaded.csv"
+    wdpa_release     = FactoryGirl.create(:wdpa_release)
+    designation_type = FactoryGirl.create(:designation_type, name: "National")
+    designation      = FactoryGirl.create(:designation, name: "National Park",
+                                          designation_type: designation_type)
+
+    CSV.stubs(:foreach).multiple_yields([CSV_CONTENT[0]], [CSV_CONTENT[1]])
+
+    Importers::Csv.import(path, wdpa_release)
+
+    assert_equal designation, ProtectedArea.find_by_wdpa_id(123).designation
+    assert_equal designation, ProtectedArea.find_by_wdpa_id(234).designation
+  end
+end
+
