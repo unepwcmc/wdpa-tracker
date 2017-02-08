5e431a48e7fb19b3bda0d6e1fa57f6dfcdb93caf
Stuart Watson <stuart.watson@unep-wcmc.org>
2017-02-08 15:27:17 +0000

    Fixed problem with deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 
-set :linked_files, %w{config/database.yml config/config.yml .env}
+set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
