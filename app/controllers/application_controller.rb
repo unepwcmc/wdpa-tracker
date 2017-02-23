9bb4a671b220410761c481cb698b148796a9ccf2
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-02-23 17:28:01 +0000

    add PageNotFound exception in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  class PageNotFound < StandardError; end;
   protect_from_forgery with: :exception
+
+  def raise_404
+    raise PageNotFound
+  end
+
+  rescue_from PageNotFound do
+    render_404
+  end
 end
