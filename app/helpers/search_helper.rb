2f7ca69ed41bc736b4a695a7fdb4a4adf73e3311
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-02-23 17:32:02 +0000

    add search helpers

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,13 @@
+module SearchHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == params[:sort] && params[:dir] == "asc") ? "desc" : "asc"
+    css_class = "table__column table__column--with-sorting".tap { |cl|
+      cl << " is-sorted-#{direction}" if column == params[:sort]
+    }
+
+    link_to({q: params[:q], sort: column, dir: direction}, class: css_class) do
+      concat title
+    end
+  end
+end
