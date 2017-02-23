7bf00f74968136cde4890e81b17ecd729c2bbd4b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-02-23 17:28:44 +0000

    add pretty name to wdpa_release

--- app/models/wdpa_release.rb
+++ app/models/wdpa_release.rb
@@ -2,6 +2,10 @@ class WdpaRelease < ActiveRecord::Base
   has_many :protected_areas_wdpa_releases
   has_many :protected_areas, through: :protected_areas_wdpa_releases
 
+  def pretty_name
+    name.match(/([A-Za-z]{3,4})(\d{4})/)[1..2].join(" ")
+  end
+
   def self.valid_on(datetime)
     WdpaRelease.where("valid_from <= ? AND valid_to > ?", datetime, datetime).first
   end
