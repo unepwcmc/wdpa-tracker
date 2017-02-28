119aad96dfc4681583c6e6816ecba950c9fe702b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-02-28 12:55:52 +0000

    add allocations to models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ActiveRecord::Base
   has_many :countries_protected_areas
   has_many :protected_areas, through: :countries_protected_areas
+
+  has_many :allocations
 end
--- app/models/protected_area.rb
+++ app/models/protected_area.rb
@@ -2,13 +2,19 @@ class ProtectedArea < ActiveRecord::Base
   belongs_to :designation
   belongs_to :wdpa_release
 
-  has_many :countries_protected_areas
+  has_many :countries_protected_areas, dependent: :destroy
   has_many :countries, through: :countries_protected_areas
 
   has_many :protected_areas_wdpa_releases
   has_many :wdpa_releases, through: :protected_areas_wdpa_releases
 
+  has_one :allocation
+
   def last_wdpa_version
     self.wdpa_releases.order(created_at: :desc).first.pretty_name
+  end
+
+  def only_allocated?
+    status == "allocated" && wdpa_releases.empty?
   end
 end
